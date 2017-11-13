4f746de623b36ce1e4c97fa407ab0b4d56aa3584
Jakub Sikora <ksikora@future-processing.com>
2017-11-13 09:35:12 +0000

    Filled website with proper content

--- src/scripts/components/PageContent.jsx
+++ src/scripts/components/PageContent.jsx
@@ -3,15 +3,17 @@ import ImageBlock from './ImageBlock';
 import AzureImg from '../../images/azure-icon.png';
 import PowerpointImg from '../../images/powerpoint-icon.jpg';
 import VisualStudioLogo from '../../images/vs-logo.jpg';
+import GithubImg from '../../images/github-logo.png';
 
 export default class PageContent extends React.Component {
     render() {
         return <section className="wrap-content">
             <h2>Materiały dodatkowe</h2>
-            <ImageBlock bootstrapClass="col-sm-2" image={AzureImg} backgroundColor="#0071c5" imageLink="#" imageInfo="Microsoft Azure Docs" />
             <ImageBlock bootstrapClass="col-sm-2" image={AzureImg} backgroundColor="#0071c5" imageLink="#" imageInfo="Microsoft Azure Portal" />
             <ImageBlock bootstrapClass="col-sm-2" image={VisualStudioLogo} backgroundColor="#69207a" imageLink="#" imageInfo="Visual Studio Dev Essentials" />
             <ImageBlock bootstrapClass="col-sm-2" image={PowerpointImg} backgroundColor="#ea4e25" imageLink="#" imageInfo="Presentation" />
+            <ImageBlock bootstrapClass="col-sm-2" image={AzureImg} backgroundColor="#0071c5" imageLink="#" imageInfo="Microsoft Azure Docs" />
+            <ImageBlock bootstrapClass="col-sm-2" image={GithubImg} backgroundColor="white" imageLink="https://github.com/needcash/to-the-cloud-materials" imageInfo="This website code" />
         </section>
     }
 }
--- src/scripts/components/Thanks.jsx
+++ src/scripts/components/Thanks.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import NewWebApp from '../../images/new-web-app.png';
+import ToFtp from '../../images/to-ftp.png';
+import ReadyWebsite from '../../images/ready-website.png';
 
 export default class Thanks extends React.Component {
 	render() {
@@ -9,7 +12,40 @@ export default class Thanks extends React.Component {
 			<p>Kuba</p>
 			<h3>Podsumowanie prezentacji</h3>
 			<ul>
-				<li>TODO</li>
+				<li>Umiejętność korzystania z chmury publicznej, wśród programistów, jest często mile widziana przez pracodawców. Korzysta z niej wiele firm w naszym regionie i na całym świecie. Chmura publiczna dostarczana jest przez m. in. Microsoft, Google, Amazon. Niezależnie od "gałęzi" programowania, który Cię interesuje - znajomość chmury publicznej zawsze będzie przydatna.</li>
+				<li>Zrobienie pierwszego kroku do nabycia tego skilla jest bardzo proste. Aby zacząć wystarczy prosta stronka jak ta i przejście 3 szybkich kroków:
+					<div className="row centered">
+						<div className="col-md-4 col-md-offset-4">
+							<div className="thumbnail">
+								<img src={NewWebApp} alt="New Web App" />
+								<div className="caption">
+								<p>Tworzymy nową stronę na Azure</p>
+							</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-4 col-md-offset-4">
+							<div className="thumbnail">
+								<img src={ToFtp} alt="To Ftp" />
+								<div className="caption">
+								<p>Wrzucamy na FTP</p>
+							</div>
+							</div>
+						</div>
+					</div>
+					<div className="row">
+						<div className="col-md-4 col-md-offset-4">
+							<div className="thumbnail">
+								<img src={ReadyWebsite} alt="Strona Gotowa" />
+								<div className="caption">
+								<p>Strona gotowa</p>
+							</div>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>Pamiętacie jak mówiłem Wam, że chmura oszczedzi Wam masę czasu? Wiadomo, że my programiści wolimy programować, a nie konfigurować i użerać się z infrastrukturą. Chmura publiczna to usługa, która po prostu działa, a jak nie działa to nie Twój problem. Wypróbuj przy swoim nastepnym projekcie zaliczeniowym i zobacz jak łątwiej i szybciej się tworzy aplikacje.</li>
 			</ul>
 		</section>
 	}
